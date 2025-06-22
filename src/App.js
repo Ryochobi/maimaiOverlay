77ac043ec5fae19a843187d4728b0f0958a021f1
Ryochobi <ryouran27@gmail.com>
2025-06-22 16:08:10 +0800

    Reroute landing to /control page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Control from './components/Control';
 
 function App() {
-  return <Control />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/control" element={<Control />} />
+        {/* Redirect anything else to /control */}
+        <Route path="*" element={<Navigate to="/control" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
